84e23be94e3c370af498298138f9850dc6147dce
Matteo Morganti <matteo99morganti@gmail.com>
2023-02-03 12:07:09 +0100

    Pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { registerLocaleData } from '@angular/common';
+import localIt from '@angular/common/locales/it';
+registerLocaleData(localIt);
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UcfirstPipe } from './ucfirst.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UcfirstPipe
   ],
   imports: [
     BrowserModule,
